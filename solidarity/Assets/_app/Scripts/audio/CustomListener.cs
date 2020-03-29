7761ad577b3a517bcebcf0603c8d61750fa195e2
Alessandro <26153311+alessandrofama@users.noreply.github.com>
2020-03-29 12:18:02 +0200

    Audio Listener Skript.

---
+++ solidarity/Assets/_app/Scripts/audio/CustomListener.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomListener : MonoBehaviour
+{
+  [SerializeField]
+  private Transform player, cam;
+
+  // Spieler 1/links = 1
+  // Spieler 2/rechts = 2
+  [SerializeField]
+  private int listener;
+
+  FMOD.ATTRIBUTES_3D attributes = new FMOD.ATTRIBUTES_3D();
+
+  FMOD.VECTOR playerPosition;
+
+  void Update()
+  {
+    attributes.forward = FMODUnity.RuntimeUtils.ToFMODVector(cam.forward);
+    attributes.up = FMODUnity.RuntimeUtils.ToFMODVector(cam.up);
+    attributes.position = FMODUnity.RuntimeUtils.ToFMODVector(cam.position);
+    playerPosition = FMODUnity.RuntimeUtils.ToFMODVector(player.position);
+
+    FMODUnity.RuntimeManager.StudioSystem.setListenerAttributes(listener, attributes, playerPosition);
+  }
+}
