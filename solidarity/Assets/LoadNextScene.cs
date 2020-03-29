863e9e7c75990ce635ee74903a8f8641e93ba294
simon.sommer <simon.sommer@byterockers.games>
2020-03-30 00:26:46 +0200

    Intro and Instructions

---
+++ solidarity/Assets/LoadNextScene.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadNextScene : MonoBehaviour
+{
+
+  public string sceneName;
+
+  void Start()
+  {
+
+  }
+
+  void Update()
+  {
+    if (Input.anyKeyDown)
+    {
+      LoadTheNextScene();
+    }
+  }
+
+  void LoadTheNextScene()
+  {
+    SceneManager.LoadScene(sceneName);
+  }
+}
