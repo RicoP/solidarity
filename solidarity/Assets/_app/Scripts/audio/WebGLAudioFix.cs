662ffd5b336442bfb83024f7914e0cee9f9cd0ba
Alessandro <26153311+alessandrofama@users.noreply.github.com>
2020-03-29 13:02:34 +0200

    Bei einer WebGL Build muss ResumeAudio() in WebGLAudioFix.cs gecallt werden, am besten durch eine User-Interaktion (Start-Button oder ähnlich). Die meisten Browser blocken mittlerweile das automatische Abspielen von Sound ohne User-Interaktion. Daraus folgt: Spieler in den Comments beschweren sich über fehlenden Sound. Das ist hier ein Fix, was die WebGL Audiotreiber mehr oder weniger neu startet..

---
+++ solidarity/Assets/_app/Scripts/audio/WebGLAudioFix.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WebGLAudioFix : MonoBehaviour
+{
+  bool audioResumed = false;
+  public void ResumeAudio()
+  {
+    if (!audioResumed)
+    {
+      var result = FMODUnity.RuntimeManager.CoreSystem.mixerSuspend();
+
+      if (Debug.isDebugBuild && result != FMOD.RESULT.OK)
+      {
+        Debug.LogWarning(result);
+      }
+
+      result = FMODUnity.RuntimeManager.CoreSystem.mixerResume();
+
+      if (Debug.isDebugBuild && result != FMOD.RESULT.OK)
+      {
+        Debug.LogWarning(result);
+      }
+
+      audioResumed = true;
+    }
+  }
+}
